e10e9667a483986ae62416ce63fd27a3a05ff6e0
francisgrane <120190258+francisgrane@users.noreply.github.com>
2024-05-25 23:55:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.51.1"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-2"
+}
 resource "aws_s3_bucket" "testbucket" {
   bucket = "testbucket6764537u87564356"
 
